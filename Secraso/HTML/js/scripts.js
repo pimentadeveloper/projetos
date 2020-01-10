bbdda7217cfbe8afedce141d9eaf71faf445c33f
Marcos Pimenta <pimentadeveloper@gmail.com>
2020-01-10 12:40:57 -0300

    Secraso - primeiro commit

---
+++ Secraso/HTML/js/scripts.js
@@ -0,0 +1,25 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    smartSpeed:1500,
+    autoplayHoverPause:true/*,
+    nav:true*/
+});
+
+$(".owl-nav").addClass("container");
+
+$("#btn-collapse").click(function() {
+    if ($(this).hasClass('collapsed')) {
+        $(this).find('svg').attr('data-icon', 'minus');
+    } else {
+        $(this).find('svg').attr('data-icon', 'plus');
+    }
+});
+
+$('#click_advance').click(function() {
+    $('#display_advance').toggle(300);
+    $("svg", this).toggleClass("fa-arrow-down fa-arrow-up");
+});
